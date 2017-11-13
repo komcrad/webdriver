b2dcd3155f0c2e9a98a981913424c3a3f76c0042
Konrad Tallman <konradt95@gmail.com>
2017-11-12 21:04:10 -0600

    Adds more unit tests. Exposes issue with the unfocus function and firefox

--- test/glados_webdriver/core_test.clj
+++ test/glados_webdriver/core_test.clj
@@ -3,15 +3,6 @@
             [glados-webdriver.core :refer :all]
             [clojure.java.io :as io]))
 (def test-html-file-url (str "file://" (.getCanonicalPath (io/file "test/resources/index.html"))))
-(defn with-all-drivers
-  "expects a function that excepts driver as an argument"
-  [f]
-  (let [driver (create-driver :chrome ["--headless"])]
-    (f driver)
-    (driver-quit driver))
-  (let [driver (create-driver :firefox ["--headless"])]
-    (f driver)
-    (driver-quit driver)))
 
 (deftest ^:parallel create-driver-test
   (testing "create-driver"
@@ -32,9 +23,9 @@
 (deftest ^:parallel to-test
   (testing "to"
     (with-all-drivers
-      (fn [driver]
-        (to driver "https://google.com")
-        (is (= "Google Search" (get-element-value driver :name "btnK" :value)))))))
+      ["--headless"]
+      (to driver "https://google.com")
+      (is (= "Google Search" (get-element-value driver :name "btnK" :value))))))
 
 (deftest ^:parallel driver-quit-test
   (testing "driver-quit"
@@ -48,27 +39,36 @@
 (deftest ^:parallel get-elements-test
   (testing "get-elements"
     (with-all-drivers
-      (fn [driver]
-        (to driver test-html-file-url)
-        (is (= 1 (count (get-elements driver :name "p1"))))
-        (is (= 2 (count (get-elements driver :id "p2"))))
-        (is (= 3 (count (get-elements driver :linkText "paragraph 3"))))
-        (is (= 4 (count (get-elements driver :className "p4"))))
-        (is (= 5 (count (get-elements driver :xpath "//p[@xpath='p5']"))))
-        (is (= 6 (count (get-elements driver :text "paragraph 6"))))
-        (is (= 7 (count (get-elements driver :tagName "paragraph"))))))))
+      ["--headless"]
+      (to driver test-html-file-url)
+      (is (= 1 (count (get-elements driver :name "p1"))))
+      (is (= 2 (count (get-elements driver :id "p2"))))
+      (is (= 3 (count (get-elements driver :linkText "paragraph 3"))))
+      (is (= 4 (count (get-elements driver :className "p4"))))
+      (is (= 5 (count (get-elements driver :xpath "//p[@xpath='p5']"))))
+      (is (= 6 (count (get-elements driver :text "paragraph 6"))))
+      (is (= 7 (count (get-elements driver :tagName "paragraph")))))))
 
 (deftest ^:parallel get-element-test
   (testing "get-element"
     (with-all-drivers
-      (fn [driver]
-        (to driver test-html-file-url)
-        (is (= "paragraph 1" (.getText (get-element driver :name "p1"))))))))
+      ["--headless"]
+      (to driver test-html-file-url)
+      (is (= "paragraph 1" (.getText (get-element driver :name "p1")))))))
 
 (deftest ^:parallel focused-element-test
   (testing "focused-element")
   (with-all-drivers
-    (fn [driver]
-      (to driver test-html-file-url)
-      (click driver :id "btn1")
-      (is (= "Button 1" (get-element-value driver (focused-element driver) :text))))))
+    ["--headless"]
+    (to driver test-html-file-url)
+    (click driver :id "btn1")
+    (is (= "Button 1" (get-element-value driver (focused-element driver) :text)))))
+
+(deftest ^:parallel unfocus-test                                     
+    (testing "unfocus")                                                
+      (with-all-drivers
+        ["--headless"]
+        (to driver test-html-file-url)
+        (click driver :id "btn1")
+        (unfocus driver)
+        (is (not (= "Button 1" (get-element-value driver (focused-element driver) :text))))))
