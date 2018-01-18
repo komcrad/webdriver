592f75ac390e212c5360d3edc4625720a1e6c3a5
Konrad Tallman <konradt95@gmail.com>
2018-01-17 20:15:03 -0600

    Version bump and project.clj license notice change

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject webdriver "0.1.1"
+(defproject webdriver "0.2.0"
   :description "A clojure selenium webdriver wrapper"
   :url "https://github.com/komcrad/webdriver"
-  :license {:name "GPL-2.0"
-            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
+  :license {:name "LGPL-3.0"
+            :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.seleniumhq.selenium/selenium-java "3.7.1"]
                  [io.github.bonigarcia/webdrivermanager "1.7.2"]
