3e5b33ecb645881e8427dbd3fbecba76e8345840
komcrad <konradt95@gmail.com>
2019-01-30 16:55:38 -0600

    Updates project.clj to current version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webdriver "0.7.0"
+(defproject webdriver "0.8.0-SNAPSHOT"
   :description "A clojure selenium webdriver wrapper"
   :url "https://github.com/komcrad/webdriver"
   :license {:name "LGPL-3.0"
