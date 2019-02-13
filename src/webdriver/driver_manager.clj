5a655603b750ece6ad7a7f11f9828deb70d5c753
komcrad <konradt95@gmail.com>
2019-02-13 17:56:33 -0600

    Adds better support for downloading files

--- src/webdriver/driver_manager.clj
+++ src/webdriver/driver_manager.clj
@@ -1,19 +1,30 @@
 (ns webdriver.driver-manager
   (:gen-class))
 
+(defn- mkdownload-dir [m]
+  (let [dir (:download-dir m)]
+    (if dir
+      (do (.mkdir (clojure.java.io/file dir))
+          (if (not (.isDirectory (clojure.java.io/file dir)))
+            (throw (Exception. ":download-dir must be a folder."))
+            (.getCanonicalPath (clojure.java.io/file dir))))
+      (System/getProperty "user.dir"))))
+
 (defn create-chrome-driver
   "creates a chromedriver and passes in vector args as command line arguments"
-  [args]
+  [m]
   (. System setProperty "wdm.targetPath" ".webdrivers/")
   (. System setProperty "webdriver.chrome.silentOutput", "true")
   (. (. (. io.github.bonigarcia.wdm.ChromeDriverManager getInstance) version "2.45") setup)
   (let [options (new org.openqa.selenium.chrome.ChromeOptions)
         capabilities (. org.openqa.selenium.remote.DesiredCapabilities chrome)]
-    (. options addArguments (concat args ["--window-size=1920x1080"]))
+    (. options addArguments (concat (if (:driver-args m) (:driver-args m) [])
+                                    ["--window-size=1920x1080"]))
     (.setExperimentalOption options "prefs"
                             (doto (new java.util.HashMap)
                              (.put "profile.default_content_settings.popups" 0)
-                             (.put "download.default_directory" ".")))
+                             (.put "download.default_directory"
+                                   (mkdownload-dir m))))
     (.setCapability capabilities (. org.openqa.selenium.remote.CapabilityType ACCEPT_SSL_CERTS) true)
     (.setCapability capabilities (. org.openqa.selenium.remote.CapabilityType ACCEPT_INSECURE_CERTS) true)
     (.setCapability capabilities (. org.openqa.selenium.chrome.ChromeOptions CAPABILITY) options)
@@ -21,7 +32,7 @@
 
 (defn create-firefox-driver
   "creates a firefoxdriver and passes in vector args as command line arguments"
-  [args]
+  [m]
   (. System setProperty "wdm.targetPath" ".webdrivers/")
   (. System setProperty "wdm.geckoDriverVersion" "0.24.0")
   (. System setProperty "webdriver.firefox.marionette" "true")
@@ -31,11 +42,11 @@
         capabilities (. org.openqa.selenium.remote.DesiredCapabilities firefox)
         profile (new org.openqa.selenium.firefox.FirefoxProfile)]
     (.setPreference profile "browser.download.folderList" 2)
-    (.setPreference profile "browser.download.dir" (System/getProperty "user.dir"))
+    (.setPreference profile "browser.download.dir" (mkdownload-dir m))
     (.setPreference profile "browser.helperApps.alwaysAsk.force" false)
     (.setPreference profile "browser.helperApps.neverAsk.saveToDisk"
-                    "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml,csv,zip,application/x-download")
-    (. options addArguments args)
+                    "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml,csv,zip,application/x-download,application/zip")
+    (. options addArguments (if (:driver-args m) (:driver-args m) []))
     (. options setProfile profile)
     (.setCapability capabilities (. org.openqa.selenium.firefox.FirefoxOptions FIREFOX_OPTIONS) options)
     (new org.openqa.selenium.firefox.FirefoxDriver capabilities)))
